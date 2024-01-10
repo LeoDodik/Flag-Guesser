10ff4cc92ae9db62438f69eb0c188bcca34dc7b5
LeoDodik <119758958+LeoDodik@users.noreply.github.com>
2024-01-10 15:20:18 +0100

    Update index.js
    DELETED

--- index.js
+++ index.js
@@ -5,13 +5,7 @@ import pg from "pg"
 const app = express();
 const port = 3000;
 
-const db =  new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database:"world",
-  password:"krmak123",
-  port:5432
-});
+
 
 db.connect()
 let quiz = db
